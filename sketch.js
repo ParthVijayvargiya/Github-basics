ddc0b987f6e4afe7e137738c4720c3f1468145ed
Parth The Coder <69713799+ParthVijayvargiya@users.noreply.github.com>
2020-09-22 16:53:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var bananaImage
+var obstacleImage , obstacleGroup
+var background
+var score
+
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  
+  banana = createSprite(400,400);
+}
+
+function draw() {
+  background(220);
+}
